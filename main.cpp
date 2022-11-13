ab4cde0bec49511fcc10221f72de6b9e60f1bd21
rohit.chakraborty <rohit.chakraborty@servicenow.com>
2022-11-13 12:16:43 +0530

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+int main() {
+    #ifdef LOCAL_RUN
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+
+    return 0;
+}
